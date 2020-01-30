eb3c57ae4cdde9bdf45bed185aaa7367ffc86e05
Fernando Britto <me@fernandobritto.com.br>
2020-01-29 22:45:54 -0200

    Initial Commit

--- resources/views/contents/litecoin.blade.php
+++
@@ -1,9 +0,0 @@
-@extends('index')
-@section('title', 'Litecoin')
-
-@section('content')
-    <div class="jumbotron text-center">
-        <h1>@yield('title')</h1>
-        <p>Problema: Moeda substituta do Bitcoin, uma evolução do Bitcoin.</p>
-    </div>
-@endsection
