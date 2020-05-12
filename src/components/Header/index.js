6da2d55527c34f46a958fead37b13be6ed3900be
Fernando Britto <fernandobritto.web@gmail.com>
2020-05-11 21:01:13 -0300

    Initial commit

--- src/components/Header/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import './styles.css'
-
-const Header = () => 
-    <header id="main-header">Produkt Katalog</header>
-
-
-export default Header
--- src/services/api.js
+++
@@ -1,3 +0,0 @@
-import axios from 'axios'
-const api = axios.create({ baseURL: 'http://localhost:2020/api'})
-export default api
